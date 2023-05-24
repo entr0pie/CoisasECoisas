9fbf0588e03184bcf60a3907892d907d108c889a
tandera@notemail.com <email@email.com>
2023-05-23 22:37:02 -0300

    Setup básico do projeto

---
+++ coisas_e_coisas/src/main/java/up/edu/br/bsi/coisas_e_coisas/App.java
@@ -0,0 +1,13 @@
+package up.edu.br.bsi.coisas_e_coisas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ coisas_e_coisas/src/main/java/up/edu/br/bsi/coisas_e_coisas/Entry.java
@@ -0,0 +1,7 @@
+package up.edu.br.bsi.coisas_e_coisas;
+
+public class Entry {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
